d1242f225fc4512ec1070f4b5133912c872dbe80
ffcosmos <1278547744@qq.com>
2026-07-21 00:55:19 +0800

    移除 Awesome-LLM 项目展示

--- assets/js/db/project.js
+++ assets/js/db/project.js
@@ -2,6 +2,7 @@ async function findAllProjects() {
     const query = `
         select *
         from project
+        where github_repo <> 'MLNLP-World/Awesome-LLM'
         order by stars desc;
     `;
     return await execute(query);
